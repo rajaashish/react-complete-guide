78f4be057104571d57347f89c5d21387bb033655
Aashish Sharma <aasyspoudyal@hotmail.com>
2018-12-31 12:41:08 -0600

    react app creating file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+
+import './App.css'
+import Person from './Person/Person'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <h1>Hi I am react app</h1>
+        <p>This is Really Working!!</p>
+        <Person />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
