ea946b718bc1711b63865bb59bf42edf566cd853
Aashish Sharma <aasyspoudyal@hotmail.com>
2019-01-01 16:18:11 -0600

    changing button style

--- src/App.js
+++ src/App.js
@@ -39,10 +39,11 @@ class App extends Component {
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
-      border: '1x solid blue',
+      border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
     }
+
     return (
       <div className='App'>
         <h1>Hi, I'm a React App</h1>
